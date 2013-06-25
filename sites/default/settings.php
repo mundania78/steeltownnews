465e76e2b3aa528dbb6fe7b8dcb7d42622df610f
Joshua Powell <joshua@viable.io>
2013-06-25 10:43:20 -0400

    Removed viable industries information from the theme;

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -274,7 +274,7 @@ $drupal_hash_salt = '';
  * It is not allowed to have a trailing slash; Drupal will add it
  * for you.
  */
-$base_url = 'http://proposal.viable.io/psu';  // NO trailing slash!
+$base_url = 'http://proposal.viable.io/steeltownnews';  // NO trailing slash!
 
 /**
  * PHP settings:
